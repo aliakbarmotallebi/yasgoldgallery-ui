1faa5b527aa8b7112c9a607871e68535d0ef3b9d
motallebi.aliakbar <motallebi.aliakbar@gmail.com>
2023-01-08 17:34:01 +0330

    nav header icons

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,12 +2,39 @@ const Header = ({children}) => {
     return (
         <header className="flex flex-none items-center h-16 bg-gray-900 shadow-sm fixed top-0 right-0 left-0 z-30">
             <div className="flex justify-between container xl:max-w-6xl mx-auto px-4 lg:px-8">
-            
-                <div className="flex items-center">
-                    <a href="" className="group inline-flex items-center space-x-2 font-bold text-lg tracking-wide text-blue-400 hover:text-blue-500">
-                        <svg className="hi-solid hi-cursor-click inline-block w-5 h-5 text-blue-200 group-hover:text-blue-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M6.672 1.911a1 1 0 10-1.932.518l.259.966a1 1 0 001.932-.518l-.26-.966zM2.429 4.74a1 1 0 10-.517 1.932l.966.259a1 1 0 00.517-1.932l-.966-.26zm8.814-.569a1 1 0 00-1.415-1.414l-.707.707a1 1 0 101.415 1.415l.707-.708zm-7.071 7.072l.707-.707A1 1 0 003.465 9.12l-.708.707a1 1 0 001.415 1.415zm3.2-5.171a1 1 0 00-1.3 1.3l4 10a1 1 0 001.823.075l1.38-2.759 3.018 3.02a1 1 0 001.414-1.415l-3.019-3.02 2.76-1.379a1 1 0 00-.076-1.822l-10-4z" clip-rule="evenodd"></path></svg>
-                        <span className="hidden sm:inline">Analytics Pro</span>
-                    </a>
+
+
+                <div class="flex items-center space-x-2 space-x-reverse">
+                    <nav className="hidden lg:flex lg:items-center lg:space-x-2 text-sm lg:space-x-reverse">
+                        <a href="" className="font-medium flex items-center space-x-2 px-3 py-2 rounded text-gray-300 bg-gray-800">
+                            صفحه اصلی
+                        </a>
+                        <a href="javascript:void(0)" className="font-medium flex items-center space-x-2 px-3 py-2 rounded text-gray-400 hover:text-gray-300 hover:bg-gray-800">
+                            درباره ما
+                        </a>
+                        <a href="javascript:void(0)" className="font-medium flex items-center space-x-2 px-3 py-2 rounded text-gray-400 hover:text-gray-300 hover:bg-gray-800">
+                            تماس با ما
+                        </a>
+                        <button type="button" class="text-sm font-medium inline-flex justify-center items-center space-x-2 space-x-reverse px-3 py-2 rounded text-gray-400 hover:text-gray-300 hover:bg-gray-800 focus:outline-none focus:ring" id="tk-dropdown-layouts-user" >
+                            <span>
+                                محصولات
+                            </span>
+                            <svg class="hi-solid hi-chevron-down inline-block w-4 h-4 opacity-50" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
+                        </button>
+                    </nav>
+                </div>
+
+                <div>
+                    <div class="relative inline-block">
+                        <button class="py-2 rounded px-2 hover:bg-white/20 transition duration-200 text-white">
+                            <svg xmlns="http://www.w3.org/2000/svg" class="w-5 h-5 fill-current" viewBox="0 0 24 24" ><path fill="none" d="M0 0h24v24H0z"/><path d="M5 18h14v-6.969C19 7.148 15.866 4 12 4s-7 3.148-7 7.031V18zm7-16c4.97 0 9 4.043 9 9.031V20H3v-8.969C3 6.043 7.03 2 12 2zM9.5 21h5a2.5 2.5 0 1 1-5 0z"/></svg>
+                        </button>
+                    </div>
+                    <div class="relative inline-block">
+                        <button class="py-2 rounded px-2 hover:bg-white/20 transition duration-200 text-white">
+                            <svg xmlns="http://www.w3.org/2000/svg" class="w-5 h-5 fill-current" viewBox="0 0 24 24" ><path fill="none" d="M0 0h24v24H0z"/><path d="M4 22a8 8 0 1 1 16 0h-2a6 6 0 1 0-12 0H4zm8-9c-3.315 0-6-2.685-6-6s2.685-6 6-6 6 2.685 6 6-2.685 6-6 6zm0-2c2.21 0 4-1.79 4-4s-1.79-4-4-4-4 1.79-4 4 1.79 4 4 4z"/></svg>
+                        </button>
+                    </div>
                 </div>
             </div>
         </header>
