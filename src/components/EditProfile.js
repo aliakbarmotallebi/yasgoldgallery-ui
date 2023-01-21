6f71c638ee5ec7bfd50b0b1fa1957a0199f6ba2f
motallebi.aliakbar <motallebi.aliakbar@gmail.com>
2023-01-21 13:52:30 +0330

    create page list orders and payments

--- src/components/EditProfile.js
+++ src/components/EditProfile.js
@@ -1,59 +1,59 @@
 
 
 const EditProfile = () => {
-    return (
-        <div class="flex flex-col justify-between">
-        <div class="max-w-sm w-full lg:max-w-full shadow px-6 py-6 bg-white rounded-lg">
-          <form action="" method="post">
-            <div class="flex items-center justify-between mb-4 pb-4 border-b">
-                <div class="text-left">
-                    <h2 class="font-semibold text-gray-900">
-                        ویرایش اطلاعات پروفایل
-                    </h2>
-                </div>
-                <div class="text-right">
-                    <button class="bg-blue-600 hover:bg-blue-700 text-white text-xs py-1.5 px-8 rounded focus:shadow-outline shadow">
-                        ویرایش تغییرات 
-                    </button>
-                </div>
+  return (
+    <div class="flex flex-col justify-between">
+      <div class="max-w-sm w-full lg:max-w-full shadow px-6 py-6 bg-white rounded-lg border mb-4">
+        <form action="" method="post">
+          <div class="flex items-center justify-between mb-4 pb-4 border-b">
+            <div class="text-left">
+              <h2 class="font-semibold text-gray-900">
+                ویرایش اطلاعات پروفایل
+              </h2>
             </div>
-              <div class=" flex justify-center">
-                <div class="w-1/2">
-                  <div class="mb-6">
-                      <label for="fullname" class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">
-                          نام و نام خانوادگی
-                      <span class="inline-flex bg-red-500 w-1 h-1 rounded-full mr-1"></span>
-                      </label>
-                      <input type="fullname" name="fullname" id="fullname" value="" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"/>
-                  </div>
-                  <div class="mb-6">
-                      <label for="username" class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">
-                        نام کاربری
-                        <span class="inline-flex bg-red-500 w-1 h-1 rounded-full mr-1"></span>
-                      </label>
-                      <input type="username" id="username" value="09306145210"  disabled readonly class="bg-gray-200  border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"/>
-                  </div>
-                  <div class="mb-6">
-                    <label for="address" class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">
-                      آدرس محل سکونت
-                      <span class="inline-flex bg-red-500 w-1 h-1 rounded-full mr-1"></span>
-                    </label>
-                    <textarea id="address" name="address" rows="4" class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500"></textarea>
-                  </div>
-                    <div class="mb-6">
-                      <label for="code" class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">
-                        کدپستی
-                        <span class="inline-flex bg-red-500 w-1 h-1 rounded-full mr-1"></span>
-                      </label>
-                      <input type="text" name="text" id="text" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" />
-
-                    </div>
-                </div>
+            <div class="text-right">
+              <button class="bg-blue-600 hover:bg-blue-700 text-white text-xs py-1.5 px-8 rounded focus:shadow-outline shadow">
+                ویرایش تغییرات
+              </button>
+            </div>
+          </div>
+          <div class=" flex justify-center">
+            <div class="w-1/2">
+              <div class="mb-6">
+                <label for="fullname" class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">
+                  نام و نام خانوادگی
+                  <span class="inline-flex bg-red-500 w-1 h-1 rounded-full mr-1"></span>
+                </label>
+                <input type="fullname" name="fullname" id="fullname" value="" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" />
               </div>
-            </form>
-        </div>
+              <div class="mb-6">
+                <label for="username" class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">
+                  نام کاربری
+                  <span class="inline-flex bg-red-500 w-1 h-1 rounded-full mr-1"></span>
+                </label>
+                <input type="username" id="username" value="09306145210" disabled readonly class="bg-gray-200  border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" />
+              </div>
+              <div class="mb-6">
+                <label for="address" class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">
+                  آدرس محل سکونت
+                  <span class="inline-flex bg-red-500 w-1 h-1 rounded-full mr-1"></span>
+                </label>
+                <textarea id="address" name="address" rows="4" class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500"></textarea>
+              </div>
+              <div class="mb-6">
+                <label for="code" class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">
+                  کدپستی
+                  <span class="inline-flex bg-red-500 w-1 h-1 rounded-full mr-1"></span>
+                </label>
+                <input type="text" name="text" id="text" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" />
+
+              </div>
+            </div>
+          </div>
+        </form>
       </div>
-    );
+    </div>
+  );
 }
 
 export default EditProfile;
--- src/components/ListPayments.js
+++ src/components/ListPayments.js
@@ -1,2 +1,124 @@
-const ListPaymentsPage = () => {} 
-export default ListPaymentsPage;
+const ListPayments = () => {
+    return (
+        <div class="flex flex-col justify-between">
+            <div class="max-w-sm w-full lg:max-w-full shadow px-6 py-6 bg-white rounded-lg border mb-4">
+                <div class="flex items-center justify-between pb-4">
+                    <div class="text-left">
+                        <h2 class="font-semibold text-gray-900">
+                            لیست تراکنش های بانکی شما
+                        </h2>
+                    </div>
+                </div>
+                <div class=" flex justify-center">
+                    <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
+                        <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+                            <tr class="border border-gray-200">
+                                <th scope="col" class="text-right px-6 py-3">
+                                    شماره تراکنش
+                                </th>
+                                <th scope="col" class="text-center px-6 py-3">
+                                    مبلغ تراکنش
+                                    <span class="text-gray-400">
+                                        (تومان)
+                                    </span>
+                                </th>
+                                <th scope="col" class="text-center px-6 py-3">
+                                    شماره سفارش
+                                </th>
+                                <th scope="col" class="text-center px-6 py-3">
+                                    وضعیت پرداخت
+                                </th>
+                                <th scope="col" class="text-center px-6 py-3">
+                                    تاریخ ایجاد
+                                </th>
+                            </tr>
+                        </thead>
+                        <tbody>
+
+                            <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 last:border-b-0">
+                                <td class="text-right px-2 py-1 font-medium text-blue-400 dark:text-white whitespace-nowrap">
+                                    11020101014745789966523
+                                </td>
+                                <td class="px-6 py-4 text-center">
+                                    2000000
+                                    تومان
+                                </td>
+                                <td class="px-6 py-4 text-center">
+                                    101402010405040
+                                </td>
+                                <td class="px-6 py-4">
+
+                                    <span class="bg-green-100 text-green-800 text-xs font-semibold mr-2 px-2.5 py-0.5 rounded dark:bg-green-200 dark:text-green-900">
+                                        منتشر شده
+                                    </span>
+
+                                    <span class="bg-red-100 text-red-800 text-xs font-semibold mr-2 px-2.5 py-0.5 rounded dark:bg-red-200 dark:text-red-900">
+                                        منتشر نشده
+                                    </span>
+
+                                </td>
+                                <td class="px-6 py-4">
+                                    140-10-10
+                                </td>
+                            </tr>
+                            <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 last:border-b-0">
+                                <td class="text-right px-2 py-1 font-medium text-blue-400 dark:text-white whitespace-nowrap">
+                                    11020101014745789966523
+                                </td>
+                                <td class="px-6 py-4 text-center">
+                                    2000000
+                                    تومان
+                                </td>
+                                <td class="px-6 py-4 text-center">
+                                    101402010405040
+                                </td>
+                                <td class="px-6 py-4">
+
+                                    <span class="bg-green-100 text-green-800 text-xs font-semibold mr-2 px-2.5 py-0.5 rounded dark:bg-green-200 dark:text-green-900">
+                                        منتشر شده
+                                    </span>
+
+                                    <span class="bg-red-100 text-red-800 text-xs font-semibold mr-2 px-2.5 py-0.5 rounded dark:bg-red-200 dark:text-red-900">
+                                        منتشر نشده
+                                    </span>
+
+                                </td>
+                                <td class="px-6 py-4">
+                                    140-10-10
+                                </td>
+                            </tr>
+                            <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 last:border-b-0">
+                                <td class="text-right px-2 py-1 font-medium text-blue-400 dark:text-white whitespace-nowrap">
+                                    11020101014745789966523
+                                </td>
+                                <td class="px-6 py-4 text-center">
+                                    2000000
+                                    تومان
+                                </td>
+                                <td class="px-6 py-4 text-center">
+                                    101402010405040
+                                </td>
+                                <td class="px-6 py-4">
+
+                                    <span class="bg-green-100 text-green-800 text-xs font-semibold mr-2 px-2.5 py-0.5 rounded dark:bg-green-200 dark:text-green-900">
+                                        منتشر شده
+                                    </span>
+
+                                    <span class="bg-red-100 text-red-800 text-xs font-semibold mr-2 px-2.5 py-0.5 rounded dark:bg-red-200 dark:text-red-900">
+                                        منتشر نشده
+                                    </span>
+
+                                </td>
+                                <td class="px-6 py-4">
+                                    140-10-10
+                                </td>
+                            </tr>
+                            
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    );
+}
+export default ListPayments;
