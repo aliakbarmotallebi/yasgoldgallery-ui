8733683e1bb2ad55c2bfe7223b182979064a002d
motallebi.aliakbar <motallebi.aliakbar@gmail.com>
2023-01-22 16:01:23 +0330

    btn carts

--- src/components/shared/HandleCart.js
+++ src/components/shared/HandleCart.js
@@ -30,14 +30,14 @@ const HandleCart = ({ product, btnStyle, btnText, btnIcon }) => {
   const addProductInCart = () => dispatch(addToCart(product));
 
   return !!isProductInCart ? (
-    <div class="w-full md:w-auto flex flex-wrap items-center pt-8 gap-5">
-      <div className="bg-white shadow-md rounded-md overflow-hidden flex items-center">
-        <button onClick={increaseCounterProduct} className="px-2.5 py-1">
-          +
+    <div class="w-full md:w-auto flex flex-wrap items-center pt-8 gap-5 text-black ">
+      <div className="bg-white shadow-md rounded-md overflow-hidden flex items-center border">
+        <button onClick={increaseCounterProduct} className="px-3 py-2 hover:bg-gray-100 hover:opacity-50">
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="20" height="20"><path fill="none" d="M0 0h24v24H0z"/><path d="M11 11V5h2v6h6v2h-6v6h-2v-6H5v-2z"/></svg>
         </button>
-        <div className="px-8">{isProductInCart.qty}</div>
-        <button onClick={decreaseCounterProduct} className="px-2.5 py-1">
-          -
+        <div className="px-4 text-bold text-xs">{isProductInCart.qty}</div>
+        <button onClick={decreaseCounterProduct} className="px-3 py-2 hover:bg-gray-100 hover:opacity-50">
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="20" height="20"><path fill="none" d="M0 0h24v24H0z"/><path d="M5 11h14v2H5z"/></svg>
         </button>
       </div>
     </div>
