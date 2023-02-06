75460944aa8ad9b621384c805c300644494a3b31
rez-a <reza.hasani11177@gmail.com>
2023-02-06 13:09:45 +0330

    responsive aboutus finall

--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -1,18 +1,17 @@
 const AboutPage = () => {
     return (
         <>
-            <div className="bg-gray-100 px-4 xl:px-4 py-14">
-                <div className="mx-auto container text-center">
+            <div className="bg-gray-100 py-14">
+                <div className="mx-auto container text-center xl:max-w-6xl mx-auto px-4 xl:px-0">
                      <h2 className="text-4xl font-bold text-gray-900">
                          درباره گالری طلا یاس
                      </h2>
-                </div>
-
-                <div className="text-2xl text-black tracking-normal px-16 py-10 font-medium leading-loose">
+                     <div className="text-2xl text-black tracking-normal py-10 font-medium leading-loose">
                     <h3 className="font-medium text-4xl py-5 text-blue-700">
                         فسلفه ما
                     </h3>
 
+                    <p className="text-justify text-xl leading-loose">
                     من در یاس، سازه ای هنرمندانه با اصالتی ایرانی و قدمتی به اندازه تجسم و خیال هستم. اما اشتباه نکنید.
                     قصد ندارم داستانی تخیلی برای شما تعریف کنم بلکه می خواهم داستان کسب و کارمان را برایتان بازگو کنم.
                      من در حوالی سال 1370 وارد بازار شدم و کارم را در زیمنه طراحی و تولید زیورآلات طلا و جواهر آغاز کردم.
@@ -20,12 +19,16 @@ const AboutPage = () => {
                      سال ها در بازار طلاوجواهر کار کردم و تجربه زیادی بدست 
                      حالا وقت آن بود که این تجربیات را یکی کی به طلاهای دست سازی تبدیل کنم که نمونه اش در هیچ کجای ایران وجود نداشت.  
                      ساخته هایی به پشتوانه تجسم و خیال پردازی...
+                    </p>
                  
                 </div>
+                </div>
+
+                
             </div>
 
-            <div class="container flex justify-between px-2 xl:px-16 py-5">
-                <div class="mt-4 flex gap-2">
+            <div class="container flex flex-wrap justify-between xl:max-w-6xl mx-auto px-4 xl:px-0 py-5">
+                <div class="mt-4 w-full md:w-1/2 lg:w-1/3 flex gap-2">
                     <div>
                         <div class="flex items-center h-16 border-l-4 border-yellow-400"></div>
                         <div class="flex items-center h-16 border-l-4 border-gray-100"></div>
@@ -43,7 +46,7 @@ const AboutPage = () => {
                         </div>
                     </div>
                 </div>
-                <div class="mt-4 flex gap-2">
+                <div class="mt-4 w-full md:w-1/2 lg:w-1/3 flex gap-2">
                     <div>
                         <div class="flex items-center h-16 border-l-4 border-yellow-400"></div>
                         <div class="flex items-center h-16 border-l-4 border-gray-100"></div>
@@ -61,7 +64,7 @@ const AboutPage = () => {
                         </div>
                     </div>
                 </div>
-                <div class="mt-4 flex gap-2">
+                <div class="mt-4 w-full md:w-1/2 lg:w-1/3 flex gap-2">
                     <div>
                         <div class="flex items-center h-16 border-l-4 border-yellow-400"></div>
                         <div class="flex items-center h-16 border-l-4 border-gray-100"></div>
